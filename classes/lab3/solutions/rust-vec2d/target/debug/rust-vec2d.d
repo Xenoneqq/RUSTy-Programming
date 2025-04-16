7236c7469fe86b69e728035731593c4d10404bec
Xenoneqq <realfatboimlg@gmail.com>
2025-04-16 09:11:49 +0200

    Created vector2d project

---
+++ classes/lab3/solutions/rust-vec2d/target/debug/rust-vec2d.d
@@ -0,0 +1,1 @@
+/mnt/d/GitHub/projects/RUSTy-Programming/classes/lab3/solutions/rust-vec2d/target/debug/rust-vec2d: /mnt/d/GitHub/projects/RUSTy-Programming/classes/lab3/solutions/rust-vec2d/src/main.rs
